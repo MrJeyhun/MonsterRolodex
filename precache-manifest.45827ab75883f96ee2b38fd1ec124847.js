55e9d122cde8ee79be54be7913706e782954a136
Jeyhun Jeyhunzade <ceyhun.zade1998@gmail.com>
2019-12-06 06:22:48 +0400

    Updates

---
+++ precache-manifest.45827ab75883f96ee2b38fd1ec124847.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26f77756e65925d3c315df259b8c6e5a",
+    "url": "/MonsterRolodex/index.html"
+  },
+  {
+    "revision": "92f357c7fd470eda3e05",
+    "url": "/MonsterRolodex/static/css/main.01216dc6.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/MonsterRolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "92f357c7fd470eda3e05",
+    "url": "/MonsterRolodex/static/js/main.7a73bad2.chunk.js"
+  },
+  {
+    "revision": "ea87b2e94a1b817408a0",
+    "url": "/MonsterRolodex/static/js/runtime-main.03b3d24e.js"
+  }
+]);
